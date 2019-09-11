c9ba435e6107969ce6e5c424561d3e9181042200
Andy Davidoff <andy+github@disruptek.com>
2019-09-11 11:35:57 -0400

    broaden hydration

--- hydrate.nim
+++ hydrate.nim
@@ -5,8 +5,30 @@ import uri
 import spec
 import paths
 
-proc hydratePath(input: JsonNode; segments: seq[PathToken]): Option[string] =
-  ## reconstitute a path with constants and variable values taken from json
+type
+  KeyVal = tuple[key: string; val: string]
+  HydrateInputs = openarray[KeyVal] | JsonNode | seq[KeyVal]
+
+converter toHydrationInputs(input: JsonNode): seq[KeyVal] =
+  if input == nil:
+    return
+  if input.kind != JObject:
+    return
+  for kv in input.pairs:
+    var value: string
+    case kv.val.kind:
+    of JString:
+      value = kv.val.getStr
+    of JInt, JFloat, JBool:
+      value = $kv.val
+    of JNull:
+      value = ""
+    else:
+      raise newException(ValueError, "unable to render " & $kv.val.kind)
+    result.add (key: kv.key, val: value)
+
+proc hydratePath(input: openarray[KeyVal]; segments: seq[PathToken]): Option[string] =
+  ## reconstitute a path with constants and variable values from an openarray
   var head: string
   if segments.len == 0:
     return some("")
@@ -14,46 +36,47 @@ proc hydratePath(input: JsonNode; segments: seq[PathToken]): Option[string] =
   case segments[0].kind:
   of ConstantSegment: discard
   of VariableSegment:
-    if head notin input:
-      return
-    let js = input[head]
-    case js.kind:
-    of JString:
-      head = js.getStr
-    of JInt, JFloat, JBool:
-      head = $js
-    of JNull:
-      head = ""
-    else:
+    block found:
+      for kv in input:
+        if head == kv.key:
+          head = kv.val
+          break found
       return
   var remainder = input.hydratePath(segments[1..^1])
   if remainder.isNone:
     return
   result = some(head & remainder.get)
 
-proc hydrateTemplate*(path: string; values: JsonNode): Option[string] =
-  ## hydrate an arbitrary string with values from a Json object
+proc hydrateTemplate*(path: string; values: openarray[KeyVal]): Option[string] =
+  ## hydrate an arbitrary string with provided values
   var
     parsed: TemplateParse
 
-  if values == nil or values.kind != JObject:
+  if values.len == 0:
     if path.isTemplate:
-      raise newException(ValueError, "values JObject required to populate template")
+      raise newException(ValueError, "values required to populate template")
     return some(path)
 
   parsed = parseTemplate(path)
 
-  # add some assertions
   for segment in parsed.segments:
     if segment.kind != VariableSegment:
       continue
     var
       msg = "path template references an unknown variable `" & $segment & "`"
-    assert $segment in values, msg
+    block found:
+      for kv in values:
+        if kv.key == $segment:
+          break found
+      raise newException(ValueError, msg)
 
   result = hydratePath(values, parsed.segments)
 
-proc hydrateUri*(uri: Uri; values: JsonNode): Option[Uri] =
+proc hydrateTemplate*(path: string; values: JsonNode): Option[string] =
+  ## hydrate an arbitrary string with provided JObject values
+  result = hydrateTemplate(path, values.toHydrationInputs)
+
+proc hydrateUri*(uri: Uri; values: HydrateInputs): Option[Uri] =
   ## hydrate a Uri's host and path fields
   var success = uri
   let
